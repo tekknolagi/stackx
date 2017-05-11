742ca331e1aa6556a19b4cada7ae2fd11393a04f
Maxwell Bernstein <tekknolagi@gmail.com>
2017-05-11 18:08:44 +0200

    Make the code more interesting

--- ast.ml
+++ ast.ml
@@ -40,8 +40,11 @@ let () =
   let open TypedAst in
   let prog = Prog
     [
+      Fun ("add2", ["a", Type.Int], Type.Int, [
+        Return (InfixOper (Plus, Var "a", IntLit 2))
+      ]);
       Fun ("main", [], Type.Int, [
-        Return (IntLit 0)
+        Return (Funcall ("add2", [IntLit 0]))
       ])
     ] in
     ignore prog
