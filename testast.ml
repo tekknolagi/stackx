e63c023719578325a0fbb13ea79e313bfe500e23
Maxwell Bernstein <tekknolagi@gmail.com>
2017-05-31 16:50:39 +0200

    fuck yes polymorphic variants

---
+++ testast.ml
@@ -0,0 +1,19 @@
+type op = [ `Plus | `Minus ]
+type exp = [
+  | `Lit of int
+  | `InfixOper of op * exp * exp
+  | `PrefixOper of [`Minus] * exp
+]
+
+let rec transform = function
+  | `Lit i -> `Lit i
+  | `InfixOper (o, e1, e2) -> `InfixOper (o, transform e1, transform e2)
+  | `PrefixOper (`Minus, e) -> `InfixOper (`Minus, `Lit 0, transform e)
+
+let rec eval = function
+  | `Lit i -> i
+  | `InfixOper (`Plus, e1, e2) -> (eval e1) + (eval e2)
+  | `InfixOper (`Minus, e1, e2) -> (eval e1) - (eval e2)
+
+let () =
+  print_endline @@ string_of_int @@ eval @@ transform @@ `PrefixOper (`Minus, `Lit 4)
