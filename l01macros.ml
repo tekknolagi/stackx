9aac8fe5811169f277326e2a4911d7bb3e5b7831
Maxwell Bernstein <tekknolagi@gmail.com>
2017-07-26 18:57:22 +0200

    Fix small error

--- l01macros.ml
+++ l01macros.ml
@@ -3,23 +3,6 @@ module AST = struct
 
   type t = [
     L00rabbit.AST.t
-    (*
-    | `Halt
-    | `Move of op2
-    | `Add of op3
-    | `Sub of op3
-    | `Mul of op3
-    | `Div of op3
-    | `Shr of op3
-    | `Shl of op3
-    | `Nand of op3
-    | `Xor of op3
-    | `Br of op1
-    | `Brz of op1
-    | `Brnz of op1
-    | `In of op1
-    | `Out of op1
-    *)
 
     | `Cmp of op2
     | `Not of op2
@@ -34,7 +17,7 @@ module AST = struct
     let lower_one = function
       | `Cmp (a, b) -> [`Sub (VM.tmp, a, b)]
       | `Not (dst, a) -> [`Nand (dst, a, a)]
-      | x -> [L00rabbit.AST.lower x]
+      | x -> L00rabbit.AST.lower [x]
     in
     List.concat @@ List.map lower_one ast
 end
