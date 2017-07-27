6be520dafa82277293ab7ada9baaf02c0cf83a80
Maxwell Bernstein <tekknolagi@gmail.com>
2017-07-27 12:05:24 +0200

    Add labels to main

--- main.ml
+++ main.ml
@@ -1,7 +1,9 @@
-let prog = L02macros.AST.lower([
+let prog : L00rabbit.AST.t list = L03labels.AST.lower [
+  `Label "start";
   `In (`Reg 1);
   `In (`Reg 2);
+  `Label "postoutput";
   `Inc (`Reg 2);
   `Cmp ((`Reg 1), (`Reg 2));
   `Out (`Reg 1)
-])
+]
